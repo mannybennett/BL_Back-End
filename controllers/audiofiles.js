8ac0ccf6aa49cef19fe442125592e53b9ee0fed9
mannybennett <mannybennett99@gmail.com>
2023-07-10 18:08:31 -0400

    Get Audio Files

--- controllers/audiofiles.js
+++ controllers/audiofiles.js
@@ -3,7 +3,7 @@ const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
 const getAllFiles = (req, res) => {
-  let sql = "SELECT file_name FROM audio_files"
+  let sql = "SELECT * FROM audio_files"
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
