cbc0310ecacf25c2f1cb539d0f9c4dd621780e40
mannybennett <mannybennett99@gmail.com>
2023-07-18 17:59:29 -0400

    Controller and routes for comments

--- controllers/limbo.js
+++ controllers/limbo.js
@@ -10,6 +10,14 @@ const getAllVotes = (req, res) => {
   })
 }
 
+const getAllComments = (req, res) => {
+  let sql = "SELECT * FROM comments"
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(rows);
+  })
+}
+
 const postVote = (req, res) => {
   const { vote, user_id, audio_file_id } = req.body;
   const sql = "INSERT INTO votes (vote, user_id, audio_file_id) VALUES (?, ?, ?)";
@@ -20,7 +28,19 @@ const postVote = (req, res) => {
   });
 };
 
+const postComment = (req, res) => {
+  const { comment, user_id, user_name, audio_file_id, profile_picture } = req.body;
+  const sql = "INSERT INTO comments (comment, user_id, user_name, audio_file_id, profile_picture) VALUES (?, ?, ?, ?, ?)";
+  const values = [comment, user_id, user_name, audio_file_id, profile_picture];
+  pool.query(sql, values, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(results);
+  });
+};
+
 module.exports = {
+  getAllVotes,
+  getAllComments,
   postVote,
-  getAllVotes
+  postComment
 }
--- routers/limbo.js
+++ routers/limbo.js
@@ -4,6 +4,10 @@ const router = express.Router()
 
 router.get('/', limboController.getAllVotes)
 
+router.get('/comments', limboController.getAllComments)
+
 router.post('/', limboController.postVote)
+
+router.post('/comments', limboController.postComment)
 
 module.exports = router
