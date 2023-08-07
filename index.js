7064bb8c45fb2bd9ece4e80aeaef4e5d1787f221
mannybennett <mannybennett99@gmail.com>
2023-08-07 13:55:04 -0400

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000
 const usersRouter = require('./routers/users')
@@ -6,6 +7,7 @@ const audioFilesRouter = require('./routers/audiofiles')
 const limboRouter = require('./routers/limbo')
 const profileRouter = require('./routers/profile')
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', usersRouter)
